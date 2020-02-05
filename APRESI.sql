cfff8c451394f3f3c6fe39074cb9f866799935e3
jefersonfagundes <jeferson.rf@outlook.com>
2020-02-04 23:26:32 -0300

    Create APRESI.sql

---
+++ APRESI.sql
@@ -0,0 +1,14 @@
+create database if not exists APRESI;
+
+USE APRESI;
+
+create table clientes(
+
+COD int not null auto_increment,
+NOME varchar(30) not null,
+UF varchar(2) not null,
+DATA_CADASTRO date not null,
+PRIMARY KEY(COD)
+);
+
+select * from clientes;
