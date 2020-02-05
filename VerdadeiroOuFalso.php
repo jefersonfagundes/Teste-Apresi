a38b2bbb2b63946cf80f0de83dbc4f662ea82e53
jefersonfagundes <jeferson.rf@outlook.com>
2020-02-04 23:24:56 -0300

    Create VerdadeiroOuFalso.php

---
+++ VerdadeiroOuFalso.php
@@ -0,0 +1,12 @@
+<?php
+
+(Verdadeiro) 0 == null
+(Verdadeiro) 10 ===(int) 10.75
+(Falso) "15" === 15
+(Falso) null === 0
+(Verdadeiro) 100 == "1e2"
+(Falso) 1 === true
+(Verdadeiro) 5 == "5"
+(Falso) null != 0
+
+?>
